71318c172878045b1f62cdd05c3458a9838f0549
stijnvanstaalduinen <144112978+stijnvanstaalduinen@users.noreply.github.com>
2024-04-03 11:35:13 +0200

    milieu pagina's + aparte css

---
+++ mil.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="homes.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" /> 
+  <link href="mil.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" /> 
+</head>
+<body>
+      <nav>
+        <div class="navimg">
+          <img src="LLC.png" alt="LLC">
+            </div>
+                <div class="nav-cont">
+                  <div class="navitem1">
+                    <a href="#">formulieren</a>
+                      <div class="navitem1-sub">
+                          <a href="klacht.php">klachten</a>
+                          <a href="comp.php">compliment</a>
+                          <a href="">info</a>
+                          </div>
+                            </div>
+                              <div class="navitem2">
+                                <a href="#">producten</a>
+                              <div class="navitem2-sub">
+                                <a href="">crud</a>
+                                <a href=""></a>
+                            </div>
+                          </div>
+                        <div class="navitem3">   
+                      <a href="#">over ons</a>
+                    <div class="navitem3-sub">
+                  <a href="">levering</a>
+                  <a href="">milieu </a>
+                  <a href=""> team</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+    <div class="mil">milieu
+   </div>
+   <div class="home-text">
+    <p>LLC is vastbesloten om het milieu te beschermen en duurzaamheid te bevorderen in al haar activiteiten. We begrijpen onze verantwoordelijkheid om de impact op het milieu te verminderen en nemen maatregelen zoals het gebruik van groene energie,
+       het verminderen van afval en het bevorderen van recycling.
+       We werken samen met leveranciers die milieuvriendelijke praktijken omarmen en ondersteunen initiatieven voor natuurbescherming in lokale gemeenschappen. Ons doel is om een positieve bijdrage te leveren aan het milieu en een duurzamere toekomst te bevorderen. </p>
+  </div>
+
+
+
+
+ 
+  
+
+
+
+  <footer>
+    <div class="footeritem1">
+      <p>
+        contact
+      </p>
+      <p>
+        tel: 0657483453
+       </p>
+       <p>
+        email: LLC@gmail.com
+       </p>
+    </div>
+    <div class="footeritem2">
+      <p>
+        locatie
+      </p>
+      <p>
+        stad:almere
+      </p>
+      <p>
+      straat:Randstad 21 nummer 77
+      </p>
+      <p>
+        <a target="_blank" href="https://www.google.com/maps/place/Randstad+21+77,+1314+BJ+Almere/@52.3772265,5.2216661,
+        17z/data=!3m1!4b1!4m6!3m5!1s0x47c6171c30a5f0bf:0xae8845b536033d5d!8m2!3d52.3772233!4d5.224241!16s%2Fg
+      %2F11bw3yy7wd?entry=ttu" class="google-maps">google maps</a>
+    </p>
+    </div>
+  </footer>
+
+  
+
+
+  
+  
+</body>
+</html>
