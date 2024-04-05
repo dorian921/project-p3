7bb7baaaad60a39b833a7d3b828d8a6d43cf3680
stijnvanstaalduinen <144112978+stijnvanstaalduinen@users.noreply.github.com>
2024-04-05 11:29:00 +0200

    team pagina

---
+++ team.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="homes.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" /> 
+  <link href="team.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" /> 
+</head>
+<body>
+      <nav>
+        <div class="navimg">
+          <img src="LLC.png" alt="LLC">
+            </div>
+                <div class="nav-cont">
+                  <div class="navitem1">
+                    <a href="#">formulieren</a>
+                      <div class="navitem1-sub">
+                          <a href="klacht.php">klachten</a>
+                          <a href="comp.php">compliment</a>
+                          <a href="">info</a>
+                          </div>
+                            </div>
+                              <div class="navitem2">
+                                <a href="#">producten</a>
+                              <div class="navitem2-sub">
+                                <a href="">crud</a>
+                                <a href=""></a>
+                            </div>
+                          </div>
+                        <div class="navitem3">   
+                      <a href="#">over ons</a>
+                    <div class="navitem3-sub">
+                  <a href="">levering</a>
+                  <a href="">milieu </a>
+                  <a href=""> team</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+    <div class="titel-homes">het LLC team
+   </div>
+   <nav class="nav2" >
+   <div class="dorian">
+    <H1>henry</H1>
+    <h2>levering</h2>
+    <p>regelt al onze leveringen</p>
+    <img id="fototeam" src="henry.jpg" alt="dorian" >
+  </div>
+  <div class="clas2">
+    <H1>jens</H1>
+    <h2>database onderhoud</h2>
+    <p>zorgt ervoor dat er niks fout gaat</p>
+    <img id="fototeam" src="jens.jpg" alt="dorian" >
+  </div>
+  <div class="clas">
+    <H1>ernard</H1>
+    <h2>verkoop</h2>
+    <p>zorgt voor de verkoop</p>
+    <img id="fototeam" src="ernard.jpg" alt="dorian" >
+  </div>
+    </nav>
+
+    <nav class="nav2">
+   <div class="dorian">
+    <H1>anouk</H1>
+    <h2>verkoop</h2>
+    <p>iemand die hard doorwerkt   </p>
+    <img id="fototeam" src="anouk.jpg" alt="dorian" >
+  </div>
+  <div class="clas2">
+    <H1>sasia</H1>
+    <h2>website design</h2>
+    <p>is een goede designer</p>
+    <img id="fototeam" src="sasia.jpg" alt="dorian" >
+  </div>
+  <div class="clas">
+    <H1>bert</H1>
+    <h2>schoonmaker</h2>
+    <p>zorgt voor een goede werkplek</p>
+    <img id="fototeam" src="bery.jpg" alt="dorian" >
+  </div>
+    </nav>
+
+
+ 
+  
+
+
+
+  <footer>
+    <div class="footeritem1">
+      <p>
+        contact
+      </p>
+      <p>
+        tel: 0657483453
+       </p>
+       <p>
+        email: LLC@gmail.com
+       </p>
+    </div>
+    <div class="footeritem2">
+      <p>
+        locatie
+      </p>
+      <p>
+        stad:almere
+      </p>
+      <p>
+      straat:Randstad 21 nummer 77
+      </p>
+      <p>
+        <a target="_blank" href="https://www.google.com/maps/place/Randstad+21+77,+1314+BJ+Almere/@52.3772265,5.2216661,
+        17z/data=!3m1!4b1!4m6!3m5!1s0x47c6171c30a5f0bf:0xae8845b536033d5d!8m2!3d52.3772233!4d5.224241!16s%2Fg
+      %2F11bw3yy7wd?entry=ttu" class="google-maps">google maps</a>
+    </p>
+    </div>
+  </footer>
+
+  
+
+
+  
+  
+</body>
+</html>
