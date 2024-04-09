19a1cfd2973fdc24be91a68c1a388e0bb7892803
Jason-Villerius <9021149@student.zadkine.nl>
2024-04-09 11:26:37 +0200

    lever pagina

---
+++ lever.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="homes.css?v=<?php echo time(); ?>" rel="stylesheet" type="text/css" /> 
+  <link href="lever.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+      <nav>
+        <div class="navimg">
+        <a href="homes.php"><img src="img/LLC.png" alt="llc"></a>
+            </div>
+                <div class="nav-cont">
+                  <div class="navitem1">
+                    <a href="#">formulieren</a>
+                      <div class="navitem1-sub">
+                          <a href="">klachten</a>
+                          <a href="">compliment</a>
+                          <a href="">info</a>
+                          </div>
+                            </div>
+                              <div class="navitem2">
+                                <a href="product.php">producten</a>
+                              <div class="navitem2-sub">
+                                
+                                <a href="klant.php">klantcrud</a>
+                                <a href="productcrud.php">productcrud</a>
+                                <a href="bestelling.php">bestelcrud</a>
+                            </div>
+                          </div>
+                        <div class="navitem3">   
+                      <a href="#">over ons</a>
+                    <div class="navitem3-sub">
+                  <a href="">levering</a>
+                  <a href="">milieu </a>
+                  <a href=""> team</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+    <div class="lever-div">
+    <p><b>Leveringsinformatie</b>
+    <br>
+    Welkom bij onze leveringsinformatiepagina. Hier vind je alle details over hoe we onze producten afleveren.</p>
+  
+    <p><b>Verzendkosten</b>
+    <br>
+    We bieden gratis verzending voor bestellingen boven een bepaald bedrag (bijvoorbeeld €50). <br>
+    Voor bestellingen onder dit bedrag wordt een standaardverzendtarief in rekening gebracht van €5,95.</p>
+  
+    <p><b>Verzendmethoden</b>
+    <br>
+    We maken gebruik van gerenommeerde verzendpartners om ervoor te zorgen dat je bestelling veilig en op tijd wordt afgeleverd. 
+    De beschikbare verzendmethoden zijn:
+    <br><br>
+    <b>Standaardverzending</b>: Levertijd van 2-4 werkdagen na verzending.<br>
+    <b>Expressverzending</b>: Levertijd van 1-2 werkdagen na verzending. Extra kosten zijn van toepassing.</p>
+
+    <p><b>Verzendgebied</b>
+    <br>
+    We verzenden momenteel alleen binnen Nederland.</p>
+  
+    <p><b>Verwerkingstijd</b>
+    <br>
+    Bestellingen worden doorgaans binnen 1-2 werkdagen verwerkt voordat ze worden verzonden. <br>
+    Houd er rekening mee dat bestellingen die in het weekend of op feestdagen worden geplaatst, mogelijk iets langer duren om te verwerken.</p>
+  
+    <p><b>Volgen van bestellingen</b>
+    <br>
+    Zodra je bestelling is verzonden, ontvang je een verzendbevestiging met trackinginformatie, zodat je de voortgang van je pakket kunt volgen.</p>
+    
+    <p><b>Vragen?</b>
+    <br>
+    Als je vragen hebt over onze leveringsinformatie of als je speciale verzoeken hebt, neem dan gerust contact met ons op via [contactgegevens].</p>
+    </div>
+    
+  
+  
+  
+  
+  
+  <footer>
+    <div class="footeritem1">
+      <p>
+        contact
+      </p>
+      <p>
+        tel: 0657483453
+       </p>
+       <p>
+        email: LLC@gmail.com
+       </p>
+    </div>
+    <div class="footeritem2">
+      <p>
+        locatie
+      </p>
+      <p>
+        stad:almere
+      </p>
+      <p>
+      straat:Randstad 21 nummer 77
+      </p>
+      <p>
+        <a target="_blank" href="https://www.google.com/maps/place/Randstad+21+77,+1314+BJ+Almere/@52.3772265,5.2216661,
+        17z/data=!3m1!4b1!4m6!3m5!1s0x47c6171c30a5f0bf:0xae8845b536033d5d!8m2!3d52.3772233!4d5.224241!16s%2Fg
+      %2F11bw3yy7wd?entry=ttu" class="google-maps">google maps</a>
+    </p>
+    </div>
+  </footer>
+ 
+</body>
+</html>
